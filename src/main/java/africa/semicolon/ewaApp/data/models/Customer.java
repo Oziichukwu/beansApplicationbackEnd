5887ac84c779c3d6ee342442cb153802df3e8112
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-16 17:51:54 +0100

    first commit

---
+++ src/main/java/africa/semicolon/ewaApp/data/models/Customer.java
@@ -0,0 +1,9 @@
+package africa.semicolon.ewaApp.data.models;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String userName;
+}
