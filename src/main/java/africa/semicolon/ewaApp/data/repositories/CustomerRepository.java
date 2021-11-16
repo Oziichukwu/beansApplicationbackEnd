0a9e3538596968b0e44e2a39dc6c166a9a9d321e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-16 18:53:48 +0100

    ewa app

--- src/main/java/africa/semicolon/ewaApp/data/repositories/CustomerRepository.java
+++ src/main/java/africa/semicolon/ewaApp/data/repositories/CustomerRepository.java
@@ -1,2 +1,15 @@
-package africa.semicolon.ewaApp.data.repositories;public interface CustomerRepository {
+package africa.semicolon.ewaApp.data.repositories;
+
+import africa.semicolon.ewaApp.data.models.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerRepository {
+
+    Customer save (Customer customer);
+    Optional<Customer> findCustomerById(String email);
+    List<Customer>findAll();
+    void deleteCustomerById(String email);
+    void delete(Customer customer);
 }
