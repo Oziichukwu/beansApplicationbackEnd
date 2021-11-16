f119cc8553ddb5a454eb916ef3c5e6250083ed4e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-16 18:53:24 +0100

    ewa app

--- src/main/java/africa/semicolon/ewaApp/data/repositories/CustomerRepositoryImpl.java
+++ src/main/java/africa/semicolon/ewaApp/data/repositories/CustomerRepositoryImpl.java
@@ -1,2 +1,42 @@
-package africa.semicolon.ewaApp.data.repositories;public class CustomerRepositoryImpl {
+package africa.semicolon.ewaApp.data.repositories;
+
+import africa.semicolon.ewaApp.data.models.Customer;
+
+import java.util.*;
+
+public class CustomerRepositoryImpl implements CustomerRepository{
+
+    Map<String, Customer> database = new HashMap<>();
+
+    @Override
+    public Customer save(Customer customer) {
+        database.put(customer.getEmail(), customer);
+        return customer;
+    }
+
+    @Override
+    public Optional<Customer> findCustomerById(String email) {
+        if (database.containsKey(email)) return Optional.of(database.get(email));
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        List<Customer>customers = new ArrayList<>();
+        Set<String>keysInDb = database.keySet();
+        for(String key : keysInDb){
+            customers.add(database.get(key));
+        }
+        return customers;
+    }
+
+    @Override
+    public void deleteCustomerById(String email) {
+        database.remove(email);
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        database.remove(customer.getEmail());
+    }
 }
