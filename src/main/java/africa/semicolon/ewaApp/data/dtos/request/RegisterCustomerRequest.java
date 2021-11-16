f118a705773a8a2579ca7e24614eefaa7a203ad6
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-16 18:54:45 +0100

    ewa app

--- src/main/java/africa/semicolon/ewaApp/data/dtos/request/RegisterCustomerRequest.java
+++ src/main/java/africa/semicolon/ewaApp/data/dtos/request/RegisterCustomerRequest.java
@@ -1,2 +1,12 @@
-package africa.semicolon.ewaApp.data.dtos.request;public class RegisterCustomerRequest {
+package africa.semicolon.ewaApp.data.dtos.request;
+
+import lombok.Data;
+
+@Data
+public class RegisterCustomerRequest {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String userName;
 }
