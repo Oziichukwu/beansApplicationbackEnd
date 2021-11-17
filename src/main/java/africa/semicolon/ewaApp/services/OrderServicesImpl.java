3ceec43d3ab5ea495280122db6c3835c30ed3ae8
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-17 01:08:54 +0100

    ewa app

--- src/main/java/africa/semicolon/ewaApp/services/OrderServicesImpl.java
+++ src/main/java/africa/semicolon/ewaApp/services/OrderServicesImpl.java
@@ -1,2 +1,28 @@
-package africa.semicolon.ewaApp.services;public class OrderServicesImpl {
+package africa.semicolon.ewaApp.services;
+
+import africa.semicolon.ewaApp.data.dtos.request.AddOrderRequest;
+import africa.semicolon.ewaApp.data.dtos.response.AddOrderResponse;
+import africa.semicolon.ewaApp.data.models.Order;
+import africa.semicolon.ewaApp.data.repositories.OrderRepository;
+import africa.semicolon.ewaApp.data.repositories.OrderRepositoryImpl;
+import africa.semicolon.ewaApp.utils.ModelMapper;
+
+import java.util.List;
+
+public class OrderServicesImpl implements OrderServices{
+    private final static OrderRepository orderRepository = new OrderRepositoryImpl();
+    @Override
+    public AddOrderResponse addOrder(AddOrderRequest addOrderRequest) {
+
+        Order order = ModelMapper.map(addOrderRequest);
+
+        Order savedOrder = orderRepository.save(order);
+
+        return ModelMapper.map(savedOrder);
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return orderRepository.findAllOrders();
+    }
 }
--- src/main/java/africa/semicolon/ewaApp/utils/ModelMapper.java
+++ src/main/java/africa/semicolon/ewaApp/utils/ModelMapper.java
@@ -1,2 +1,55 @@
-package africa.semicolon.ewaApp.utils;public class ModelMapper {
+package africa.semicolon.ewaApp.utils;
+
+import africa.semicolon.ewaApp.data.dtos.request.AddOrderRequest;
+import africa.semicolon.ewaApp.data.dtos.request.RegisterCustomerRequest;
+import africa.semicolon.ewaApp.data.dtos.response.AddOrderResponse;
+import africa.semicolon.ewaApp.data.dtos.response.RegisterCustomerResponse;
+import africa.semicolon.ewaApp.data.models.Customer;
+import africa.semicolon.ewaApp.data.models.Order;
+
+public class ModelMapper {
+
+    public static Customer map(RegisterCustomerRequest registerCustomerRequest){
+
+        Customer customer = new Customer();
+
+        customer.setFirstName(registerCustomerRequest.getFirstName());
+        customer.setLastName(registerCustomerRequest.getLastName());
+        customer.setPhoneNumber(registerCustomerRequest.getPhoneNumber());
+        customer.setUserName(registerCustomerRequest.getUserName());
+        customer.setEmail(registerCustomerRequest.getEmail());
+        customer.setPassword(registerCustomerRequest.getPassword());
+        return customer;
+    }
+
+    public static RegisterCustomerResponse map(Customer savedCustomer){
+
+        RegisterCustomerResponse response = new RegisterCustomerResponse();
+
+        response.setUserName(savedCustomer.getUserName());
+        response.setEmail(savedCustomer.getEmail());
+        return response;
+    }
+
+    public static Order map(AddOrderRequest addOrderRequest){
+
+        Order order = new Order();
+        order.setBeansType(addOrderRequest.getBeansType());
+        order.setReceiverName(addOrderRequest.getReceiverName());
+        order.setDeliveryAddress(addOrderRequest.getDeliveryAddress());
+
+        return order;
+    }
+
+    public static AddOrderResponse map(Order savedOrder){
+
+        AddOrderResponse response = new AddOrderResponse();
+
+        response.setTrackingid(savedOrder.getId());
+        response.setReceiverName(savedOrder.getReceiverName());
+        response.setDeliveryAddress(savedOrder.getDeliveryAddress());
+        response.setBeansType(savedOrder.getBeansType());
+
+        return response;
+    }
 }
