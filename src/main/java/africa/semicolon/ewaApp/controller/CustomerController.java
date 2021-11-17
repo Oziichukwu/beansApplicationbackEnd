cf465b3528555c2533272e5b57015894733c13c7
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-17 01:14:40 +0100

    ewa app

--- src/main/java/africa/semicolon/ewaApp/controller/CustomerController.java
+++ src/main/java/africa/semicolon/ewaApp/controller/CustomerController.java
@@ -1,6 +1,26 @@
 package africa.semicolon.ewaApp.controller;
 
-public class RegisterCustomerController {
+import africa.semicolon.ewaApp.data.dtos.request.RegisterCustomerRequest;
+import africa.semicolon.ewaApp.data.dtos.response.RegisterCustomerResponse;
+import africa.semicolon.ewaApp.data.models.Customer;
+import africa.semicolon.ewaApp.services.CustomerServices;
+import africa.semicolon.ewaApp.services.CustomerServicesImpl;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 
+@RestController
+public class CustomerController {
+
+    private final CustomerServices customerServices = new CustomerServicesImpl();
+
+    @PostMapping("/api/v1/sender")
+    public RegisterCustomerResponse registerCustomer(@RequestBody RegisterCustomerRequest registerCustomerRequest){
+        return customerServices.registerCustomer(registerCustomerRequest);
+    }
+
+    @GetMapping("/api/customer/{email}")
+    public Optional<Customer> findCustomerById( @PathVariable String email){
+        return customerServices.findCustomerByEmail(email);
+    }
 }
