09c45646c12c74d8760c86856047faa1f362af17
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-17 01:09:48 +0100

    ewa app

--- src/main/java/africa/semicolon/ewaApp/services/CustomerServices.java
+++ src/main/java/africa/semicolon/ewaApp/services/CustomerServices.java
@@ -1,2 +1,21 @@
-package africa.semicolon.ewaApp.services;public interface CustomerServices {
+package africa.semicolon.ewaApp.services;
+
+import africa.semicolon.ewaApp.data.dtos.request.RegisterCustomerRequest;
+import africa.semicolon.ewaApp.data.dtos.response.RegisterCustomerResponse;
+import africa.semicolon.ewaApp.data.models.Customer;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public interface CustomerServices {
+
+    RegisterCustomerResponse registerCustomer(RegisterCustomerRequest registerCustomerRequest);
+
+    List<Customer> findAllCustomers();
+
+    Optional<Customer> findCustomerByEmail(String email);
+
+    void deleteAllCustomers();
 }
--- src/main/java/africa/semicolon/ewaApp/services/CustomerServicesImpl.java
+++ src/main/java/africa/semicolon/ewaApp/services/CustomerServicesImpl.java
@@ -2,8 +2,44 @@ package africa.semicolon.ewaApp.services;
 
 import africa.semicolon.ewaApp.data.dtos.request.RegisterCustomerRequest;
 import africa.semicolon.ewaApp.data.dtos.response.RegisterCustomerResponse;
+import africa.semicolon.ewaApp.data.models.Customer;
+import africa.semicolon.ewaApp.data.repositories.CustomerRepository;
+import africa.semicolon.ewaApp.data.repositories.CustomerRepositoryImpl;
+import africa.semicolon.ewaApp.exceptions.DuplicateCustomerException;
+import africa.semicolon.ewaApp.utils.ModelMapper;
 
-public interface CustomerServices {
+import java.util.List;
+import java.util.Optional;
 
-    RegisterCustomerResponse registerCustomer(RegisterCustomerRequest registerCustomerRequest);
+public class CustomerServicesImpl implements CustomerServices{
+
+    private final static CustomerRepository customerRepository = new CustomerRepositoryImpl();
+    @Override
+    public RegisterCustomerResponse registerCustomer(RegisterCustomerRequest registerCustomerRequest) {
+
+        Optional<Customer>savedCustomerInDb = customerRepository.findCustomerById(registerCustomerRequest.getEmail());
+        if (savedCustomerInDb.isPresent()) throw new DuplicateCustomerException(registerCustomerRequest.getEmail()+ "already exist") ;
+        Customer customer = ModelMapper.map(registerCustomerRequest);
+
+        Customer savedCustomer = customerRepository.save(customer);
+
+        return ModelMapper.map(savedCustomer);
+    }
+
+    @Override
+    public List<Customer> findAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findCustomerByEmail(String email) {
+
+        return customerRepository.findCustomerById(email);
+    }
+
+    @Override
+    public void deleteAllCustomers() {
+        customerRepository.deleteAll();
+    }
+
 }
