0650e86c5ec9b331ca8e1b2fef54f96b22ae13fe
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-17 01:09:18 +0100

    ewa app

--- src/main/java/africa/semicolon/ewaApp/services/OrderServices.java
+++ src/main/java/africa/semicolon/ewaApp/services/OrderServices.java
@@ -1,2 +1,15 @@
-package africa.semicolon.ewaApp.services;public interface OrderServices {
+package africa.semicolon.ewaApp.services;
+
+import africa.semicolon.ewaApp.data.dtos.request.AddOrderRequest;
+import africa.semicolon.ewaApp.data.dtos.response.AddOrderResponse;
+import africa.semicolon.ewaApp.data.models.Order;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface OrderServices {
+
+    AddOrderResponse addOrder(AddOrderRequest addOrderRequest);
+
+    List<Order> getAllOrders();
 }
