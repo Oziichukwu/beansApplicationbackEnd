f3e86d5faa969842c53411368e0d1f9211b1fc01
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-23 23:16:43 +0100

    ewa app

--- src/main/java/africa/semicolon/ewaApp/controller/CustomerController.java
+++ src/main/java/africa/semicolon/ewaApp/controller/CustomerController.java
@@ -7,20 +7,27 @@ import africa.semicolon.ewaApp.services.CustomerServices;
 import africa.semicolon.ewaApp.services.CustomerServicesImpl;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class CustomerController {
 
     private final CustomerServices customerServices = new CustomerServicesImpl();
-
+    @CrossOrigin(origins = "http://localhost:3001/")
     @PostMapping("/api/v1/customer")
     public RegisterCustomerResponse registerCustomer(@RequestBody RegisterCustomerRequest registerCustomerRequest){
         return customerServices.registerCustomer(registerCustomerRequest);
     }
-
+    @CrossOrigin(origins = "http://localhost:3001/")
     @GetMapping("/api/customer/{email}")
     public Optional<Customer> findCustomerById( @PathVariable String email){
         return customerServices.findCustomerByEmail(email);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3001/")
+    @GetMapping("/api/customers/")
+    public List<Customer> findAllCustomers(){
+        return customerServices.findAllCustomers();
     }
 }
